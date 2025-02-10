2ce2ddc32ab9d5f1e425e93ede29869c2803e102
Pablo Fernandez <2023_pablo.fernandez@iticbcn.cat>
2025-02-10 13:15:02 +0100

    ch

--- autosigned-https/initial_conf.sh
+++ autosigned-https/initial_conf.sh
@@ -4,32 +4,19 @@
 mkdir ./{config,certs}
 
 # certificat
-curl https://traefik.me/privkey.pem -o ./certs/demo.traefik.me.key
-curl https://traefik.me/cert.pem -o ./certs/demo.traefik.me.crt
+openssl req -new -newkey rsa:4096 -days 365  -nodes -x509 -keyout ./certs/privkey.key -out ./certs/server.crt -subj '/C=SP/ST=Testing/L=Barcelona/O=ITIC/CN=localhost'
 
-chmod 600 certs/demo.traefik.me.key
-chmod 644 certs/demo.traefik.me.crt
-sudo chown root:root certs/*
+chmod 600 certs/privkey.key
+chmod 644 certs/server.crt
 
 tee ./config/https.conf << EOF
-# Put this at the very top of the file, before everything else
-ServerName demo.traefik.me
-
-# Global SSL configuration
-LoadModule ssl_module modules/mod_ssl.so
-LoadModule socache_shmcb_module modules/mod_socache_shmcb.so
-
-SSLProtocol all -SSLv3 -TLSv1 -TLSv1.1
-SSLCipherSuite HIGH:!aNULL:!MD5
-SSLSessionCache "shmcb:/usr/local/apache2/logs/ssl_scache(512000)"
-
 <VirtualHost *:443>
-    ServerName demo.traefik.me
+    ServerName localhost
     DocumentRoot /var/www/html
     
     SSLEngine on
-    SSLCertificateFile /etc/apache2/ssl/demo.traefik.me.crt
-    SSLCertificateKeyFile /etc/apache2/ssl/demo.traefik.me.key
+    SSLCertificateFile /etc/apache2/ssl/server.crt
+    SSLCertificateKeyFile /etc/apache2/ssl/privkey.key
     
     Protocols h2 http/1.1
     
